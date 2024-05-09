9318dccf37937f3625fec20fdca76a2c5cb4c7bc
unknown <xylonarroyo16@gmail.com>
2024-05-09 19:27:58 +0800

    Removed git commands

--- scripts/index.js
+++ scripts/index.js
@@ -7,11 +7,3 @@ import "./scrollAnimation.js";
 import "./tooltips.js";
 
 import "./utils.js";
-
-// echo "# ylonn.github.io" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/ylonn/ylonn.github.io.git
-// git push -u origin main
